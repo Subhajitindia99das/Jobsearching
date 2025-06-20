bac36a86f784a9f444109bf1fef02356bdf1527b
Subhajit Das <72573641+Subhajitindia99das@users.noreply.github.com>
2025-06-20 09:27:03 +0530

    The files are uploaded

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// js/script.js
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded");
+  });
+  
+// function toggleTheme() {
+//     document.body.classList.toggle('dark');
+//   }
+  
+//   function toggleMenu() {
+//     const navLinks = document.getElementById('navLinks');
+//     navLinks.classList.toggle('show');
+//   }
+  
